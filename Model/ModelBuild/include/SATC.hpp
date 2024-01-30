f87fa96ec583300ed3b717342edf8964b9192a21
lenoben <lenoben002@gmail.com>
2024-01-30 11:24:04 +0200

    added the overall header file

---
+++ Model/ModelBuild/include/SATC.hpp
@@ -0,0 +1,17 @@
+#ifndef SATC_HPP
+#define SATC_HPP
+
+#include <vector>
+#include <iostream>
+
+#include "json.hpp"
+
+using Json = nlohmann::json;
+enum FileType
+{
+    TXTGZ,
+    JSON,
+    TXT
+};
+
+#endif
