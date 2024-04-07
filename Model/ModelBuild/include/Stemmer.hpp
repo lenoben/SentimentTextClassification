c10f223fd108205dc46456b766245357871b6bc8
lenoben <lenoben002@gmail.com>
2024-04-07 15:22:23 +0200

    stemmer processlines

--- Model/ModelBuild/include/Stemmer.hpp
+++ Model/ModelBuild/include/Stemmer.hpp
@@ -32,6 +32,22 @@ public:
     void stemfile(std::string inputFile, std::string outputFile);
 };
 
+void stemm::processLines(std::vector<std::string> &lines, std::vector<std::string> &result)
+{
+    stemm s;
+    for (const auto &line : lines)
+    {
+        std::stringstream ss(line);
+        std::string word;
+        std::string stemmedLine;
+        while (ss >> word)
+        {
+            stemmedLine += s.stemWord(word) + " ";
+        }
+        result.push_back(stemmedLine);
+    }
+}
+
 stemm::stemm(/* args */)
 {
     z = create_stemmer();
