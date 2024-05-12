dcf5c95aa6e4f60539ac13eeae883ad160c0863d
lenoben <lenoben002@gmail.com>
2024-05-12 20:11:12 +0300

    ffn impl 8. predict and save

--- Model/ModelBuild/src/MB_Fnn.cpp
+++ Model/ModelBuild/src/MB_Fnn.cpp
@@ -152,6 +152,37 @@ namespace mb
         std::cout << "Train Label Matrix n_rows - " << FNN_trainLabel.n_rows << std::endl;
         std::cout << "Train Label Matrix n_cols - " << FNN_trainLabel.n_cols << std::endl;
 
+         /*
+            ****
+                Model training parameters
+            ****
+        */
+
+        // Number of epochs for training. Increase number of epochs for better results.
+        const int EPOCHS = 32;
+
+        //! - STEP_SIZE: Step size of the optimizer. learning rate also
+        constexpr double STEP_SIZE = 0.001; // 2e-5; // 0.000001; // 5e-2;
+
+        //! - BATCH_SIZE: Number of data points in each iteration of SGD.
+        constexpr int BATCH_SIZE = 16; // 32
         
+        //! - STOP_TOLERANCE: Stop tolerance;
+        // A very small number implies that we do all iterations.
+        constexpr double STOP_TOLERANCE = 1e-8;
+
+        mlpack::FFN<mlpack::MeanSquaredError, mlpack::HeInitialization> &model = mean_he((int)trainMat.n_rows);
+        
+        model.Train(trainMat, FNN_trainLabel, ens::PrintLoss(), ens::ProgressBar(40));
+
+        model.Predict(testMat, pred);
+        // File for saving the model.
+        std::string modelname = "FFN_" + encoderTypeToString(ET) + "_" + scalerMethodToString(SM) + "_" + std::to_string(opt);
+
+        mlpack::data::Save(modelname + "_nn_regressor.bin", "NNRegressor", model);
+
+        delete &model;
+        mlpack::data::Save(modelname + "_pred.csv", pred);
+        mlpack::data::Save(modelname + "_test.csv", FNN_testLabel);
     }
 }
